9d61e5818a47d5c1ff6d2ba65685e3cfebaf30fa
jimmyxuexue <1002661758@qq.com>
2022-10-30 17:23:51 +0800

    🔧 stroage config

--- src/pages/Index/index.tsx
+++ src/pages/Index/index.tsx
@@ -1,16 +1,31 @@
-import { memo, useEffect } from 'react';
+import { memo, useEffect, useState } from 'react';
 import { View, Text, Button, SafeAreaView } from 'react-native';
 import navigate from '../../navigation/navigate';
+import { getStorage, saveStorage } from '../../utils';
 
 export default memo(() => {
+  const [count, setCount] = useState<number>(0);
+
   useEffect(() => {
-    console.log(111);
+    (async () => {
+      const storageCount = await getStorage('snow-test-count');
+      setCount(+storageCount || 0);
+    })();
   }, []);
 
   return (
     <SafeAreaView>
       <View>
-        <Text>nowCount: 11</Text>
+        <Text>nowCount: {count}</Text>
+        <Button
+          onPress={async () => {
+            await saveStorage('snow-test-count', String(count + 1));
+            setCount(count + 1);
+          }}
+          title="add one"
+          color="#841584"
+          accessibilityLabel="Learn more about this purple button"
+        />
         <Button
           onPress={() => {
             navigate('Mine', {
